fe1d9b7c0bc28cd5784d0aee40be525f5459b6f4
NikonPic <nikolas@wilhelm.nu>
2021-10-25 20:47:52 +0200

    latest measurements

--- constant.py
+++ constant.py
@@ -6,21 +6,25 @@ import signal
 from datetime import datetime
 from argmax_gym import TcpRemoteGym
 import json
+from datasplit import ObservationHandler
 
 
 def millis():
     """return the time since start in milliseconds"""
     dt = datetime.now() - start_time
-    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+        1000 + dt.microseconds / 1000.0
     return ms
+
 
 def sigint_handler(signal, frame):
     print('Interrupted')
     sys.exit(0)
 
-signal.signal(signal.SIGINT, sigint_handler)
 
+signal.signal(signal.SIGINT, sigint_handler)
 batch_size = 1
+obs_handler = ObservationHandler(num_rigid_bodies=5)
 
 env = TcpRemoteGym(
     "phoenix-v0",
@@ -33,18 +37,47 @@ print('init')
 obs, image = env.reset(record=False)
 print('reset')
 
+# Motor nummern:
+
+"""
+Strecker 1 zeigerfinger: 5
+Strecker 2 zeigerfinger: 4
+
+Beuger 1 zeigerfinger: 1
+Beuger 2 zeigerfinger: 3
+
+Strecker 1 Daumen: 6
+Strecker 2 Daumen: 7
+
+Daumen Abspreitzer: 2
+Daumen Beuger: 0
+
+
+
+
+Id
+ZF PP : 1007
+ZF DP : 1008
+
+DAUMEN DP : 1009
+DAUMEN MC : 1010
+
+
+"""
+
+
 action_off = np.array([[
-#       1    2    3    4    5    6    7    8   
-        1.5, 1.5, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        #0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # 0    1    2    3    4    5    6    7
+    1, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0, 1.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    #0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 ]])
 
 action_amp = np.array([[
-#       1    2    3     4     5    6    7    8  
-        1.5, 1.5, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        #0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # 0    1    2    3    4    5    6    7
+    1.0, -1.5, 1.0, -1.5, 1.5, 1.5, -0.5, -0.5,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    #0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 ]])
 
 
@@ -54,37 +87,36 @@ start_time = datetime.now()
 
 json_out = {
     'time': [],
-    'action' : [],
-    'observation' : [],
+    'action': [],
 }
 
 
-maxtime_s = 10
+maxtime_s = 30
 millisec = 0
 printcount = 0
 
 
 while (millisec < (maxtime_s * 1000)):
     millisec = millis()
-    
+
     t += dt
-    action =  5 * action_off +  7 * action_amp * np.cos(t)
+    action = 5 * action_off + 5 * action_amp * np.cos(t)
     obs, reward, done, image = env.step(action)
 
     append_obs = obs[0].tolist()
 
-    # add to json_out:
-    json_out['time'].append(millisec)
-    json_out['action'].append(action.tolist())
-    json_out['observation'].append(append_obs)
+    if obs_handler(append_obs):
+        # add to json_out:
+        json_out['time'].append(millisec)
+        json_out['action'].append(action.tolist())
 
     printcount += 1
     if printcount > 1000:
-        print(millisec)
-        print(type(append_obs))
-        print(len(append_obs))
-        print(len(json_out['observation'][-1]))
         printcount = 0
+
+
+print('finished')
+json_out['observation'] = obs_handler.output_dict
 
 with open('latest.json', 'w') as f:
     json.dump(json_out, f, indent=2)
--- read_test.py
+++ read_test.py
@@ -2,16 +2,32 @@
 import json
 import matplotlib.pyplot as plt
 
-with open('./data.json', encoding='utf-8', errors='ignore') as f:
+
+with open('./latest.json', encoding='utf-8', errors='ignore') as f:
     data = json.load(f)
 
-#plt.plot(data['time'])
+
+def get_vec_by_id(data, key='observation', idx=0):
+    loc_data = data[key]
+    vector = [datarow[idx] for datarow in loc_data]
+    return vector
+
+
+# plt.plot(data['time'])
 # %%
 data.keys()
 # %%
-len(data['observation'][1])
+len(data['observation']['motors'][0]['power'])
 # %%
-len(data['observation'][-1])
 # %%
 plt.plot(data['time'])
+# %%
+plt.plot(data['observation']['motors'][0]['power'])
+# %%
+# %%
+plt.plot(data['observation']['analogs'][0]['force'])
+# %%
+plt.plot(data['observation']['analogs'][0]['dforce'])
+# %%
+plt.plot(data['observation']['force_torques'][0]['mx'])
 # %%
